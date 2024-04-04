8677d4bed5f07c4ffbb70ffd290cb5e57d9f7537
Daniel Jorgensen <dan.r.jorgensen@gmail.com>
2024-04-03 19:15:30 -0600

    Added custom queries to repositories

--- src/main/java/com/dbgrocerystore/demo/repository/ProductRepository.java
+++ src/main/java/com/dbgrocerystore/demo/repository/ProductRepository.java
@@ -1,9 +1,14 @@
 package com.dbgrocerystore.demo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.dbgrocerystore.demo.entity.Product;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
-    
+    List<Product> findByNameContainingIgnoreCase(String keyword);
+    List<Product> findByCategoryIgnoreCase(String category);
 }
---
+++ src/main/java/com/dbgrocerystore/demo/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.dbgrocerystore.demo.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.dbgrocerystore.demo.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUserName(String userName);
+    Optional<User> findByUserEmail(String userEmail);
+}
