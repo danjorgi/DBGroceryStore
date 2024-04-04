cabf74b052aed59972b844606ed197ec58191df0
Daniel Jorgensen <dan.r.jorgensen@gmail.com>
2024-04-03 18:38:33 -0600

    Added getter/setters to Cart entity

--- src/main/java/com/dbgrocerystore/demo/entity/Cart.java
+++ src/main/java/com/dbgrocerystore/demo/entity/Cart.java
@@ -23,4 +23,55 @@ public class Cart {
 
     @Column(name = "timestamp", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
     private Timestamp timestamp;
+
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return this.user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Product getProduct() {
+        return this.product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public int getQuantity() {
+        return this.quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public Timestamp getTimestamp() {
+        return this.timestamp;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
+
+
+    public Cart(Integer id, User user, Product product, int quantity, Timestamp timestamp) {
+        this.id = id;
+        this.user = user;
+        this.product = product;
+        this.quantity = quantity;
+        this.timestamp = timestamp;
+    }
+
 }
